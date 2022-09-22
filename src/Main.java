a3fc11fd0ce76c5b3ec47526cdc75e1ad398d48a
1E10 Laptop <sample@bths.edu>
2022-09-22 11:54:22 -0400

    it works

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        DecimalFormat formatter = new DecimalFormat("#.##");
 
         System.out.println("Welcome to the tip calculator!");
         System.out.print("How many people are in your group: ");
         int ppl = scan.nextInt();
 
         System.out.print("What's the tip precentage? (0 - 100): ");
-        double tip = scan.nextInt()/100.0;
+        int tip = scan.nextInt();
 
         double num   = 0.0;
         double total = 0.0;
@@ -20,7 +22,30 @@ public class Main {
             num = scan.nextDouble();
 
         }
-        System.out.println("The total is"+ total);
+        double totalTip =(tip/100.0)*total;
+        double ttb = (1+(tip/100.0))*total;
+        String totalFormat = formatter.format(total);
+        String totalTipFormat = formatter.format(totalTip);
+        String totalBillTip = formatter.format(ttb);
+        String ppbt = formatter.format(total/ppl);
+        String tpp = formatter.format(totalTip/ppl);
+        String tcp = formatter.format(ttb/ppl);
+
+        System.out.println("------------------------------------");
+
+        System.out.println("Total bill before Tip: "+ totalFormat);
+
+        System.out.println("Tip Percentage: "+ tip);
+
+        System.out.println("Total Tip: " + totalTipFormat);
+
+        System.out.println("Total Bill with Tip: "+totalBillTip);
+
+        System.out.println("Per Person Cost Before Tip:  "+ ppbt);
+
+        System.out.println("Tip Per Person:  "+ tpp);
+
+        System.out.println("Total Cost Per Person: "+ tcp);
 
 
         scan.close();
